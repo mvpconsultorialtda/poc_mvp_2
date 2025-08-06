662fb8ba7e7c35cb5393c95eecf6130fdd19c275
Felipe Carvalho <mvpconsultorialtda@gmail.com>
2025-08-06 13:45:45 -0300

    fix(build): correct package build output and clean up artifacts

--- packages/background-remover/src/components/ImageEditorModal.d.ts
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './../background-remover.css';
-interface ImageEditorModalProps {
-    isOpen: boolean;
-    onClose: () => void;
-    file: File | null;
-}
-declare const ImageEditorModal: React.FC<ImageEditorModalProps>;
-export default ImageEditorModal;
